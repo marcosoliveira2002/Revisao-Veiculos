8971f9fe6cd23845a83e3ae6d77328fdd58b8fc3
Marcos Oliveira <oliveiraregojunior@gmail.com>
2024-03-19 09:11:28 -0400

    Salvando alteracoes relatorio por data

--- get-revisoes.php
+++ get-revisoes.php
@@ -4,7 +4,7 @@ include_once("conexao.php");
 $dataInicial = $_GET['dataInicial'];
 $dataFinal = $_GET['dataFinal'];
 
-$sql = "SELECT r.id, r.cpf_proprietario, v.modelo, r.servico, r.valor, r.data_revisao FROM marcos.revisao r INNER JOIN marcos.veiculos v ON v.id=r.veiculo_id WHERE LEFT(dataInicial,10) BETWEEN $dataInicial AND $dataFinal ORDER BY cpf_proprietario";
+$sql = "SELECT r.id, r.cpf_proprietario, v.modelo, r.servico, r.valor, r.data_revisao FROM marcos.revisao r INNER JOIN marcos.veiculos v ON v.id=r.veiculo_id WHERE LEFT(r.data_revisao,10) BETWEEN $dataInicial AND $dataFinal ORDER BY cpf_proprietario";
 
 $result = pg_prepare($cn, "revisoes", $sql); 
 
--- imgs/JS/script.js
+++ imgs/JS/script.js
@@ -7,17 +7,17 @@ const campo4 = document.querySelector("#campo4") /* telefone*/
 const campo5 = document.querySelector("#campo5") /* Observacao*/
 
 function validarNumero(event) {
-    var charCode = event.which ? event.which : event.keyCode;
-    var codigoTecla = event.keyCode;
+  var charCode = event.which ? event.which : event.keyCode;
+  var codigoTecla = event.keyCode;
 
 
-    if ((codigoTecla >= 48 && codigoTecla <= 57) || codigoTecla == 8 || codigoTecla == 45 || charCode == 44 || charCode == 46) {
+  if ((codigoTecla >= 48 && codigoTecla <= 57) || codigoTecla == 8 || codigoTecla == 45 || charCode == 44 || charCode == 46) {
 
 
-        return true;
-    } else {
-        return false;
-    }
+    return true;
+  } else {
+    return false;
+  }
 }
 
 /* Cadastro de Revisao  */
@@ -26,111 +26,111 @@ let selectProprietario = document.getElementById('proprietarioRevisao');
 
 
 selectProprietario.onchange = function () {
-    // console.log('...', document.getElementById('campo2').value)
-    let selectVeiculo = document.getElementById('proprietarioRevisao');
-    let valor = selectVeiculo.value;
-    //        console.log('valor',valor);
-    fetch("get-veiculos.php?proprietario_cpf=" + valor)
-        .then(response => {
-            return response.text();
-        })
-        .then(texto => {
-            //console.log(texto)
-            document.getElementById('veiculoPropietario').innerHTML = texto;
-        });
+  // console.log('...', document.getElementById('campo2').value)
+  let selectVeiculo = document.getElementById('proprietarioRevisao');
+  let valor = selectVeiculo.value;
+  //        console.log('valor',valor);
+  fetch("get-veiculos.php?proprietario_cpf=" + valor)
+    .then(response => {
+      return response.text();
+    })
+    .then(texto => {
+      //console.log(texto)
+      document.getElementById('veiculoPropietario').innerHTML = texto;
+    });
 
 }
 
 
 function validaCPF(cpfProprietario) {
-    var Soma = 0
-    var Resto
-  
-    var strCPF = String(cpfProprietario).replace(/[^\d]/g, '')
-    
-    if (strCPF.length !== 11)
-       return false
-    
-    if ([
-      '00000000000',
-      '11111111111',
-      '22222222222',
-      '33333333333',
-      '44444444444',
-      '55555555555',
-      '66666666666',
-      '77777777777',
-      '88888888888',
-      '99999999999',
-      ].indexOf(strCPF) !== -1)
-      return false
-  
-    for (i=1; i<=9; i++)
-      Soma = Soma + parseInt(strCPF.substring(i-1, i)) * (11 - i);
-  
-    Resto = (Soma * 10) % 11
-  
-    if ((Resto == 10) || (Resto == 11)) 
-      Resto = 0
-  
-    if (Resto != parseInt(strCPF.substring(9, 10)) )
-      return false
-  
-    Soma = 0
-  
-    for (i = 1; i <= 10; i++)
-      Soma = Soma + parseInt(strCPF.substring(i-1, i)) * (12 - i)
-  
-    Resto = (Soma * 10) % 11
-  
-    if ((Resto == 10) || (Resto == 11)) 
-      Resto = 0
-  
-    if (Resto != parseInt(strCPF.substring(10, 11) ) )
-      return false
-  
-    return true
-  }
-  function verificarCPF() {
-    var cpfInput = document.getElementById("cpfProprietario");
-    var cpf = cpfInput.value;
+  var Soma = 0
+  var Resto
 
-    if (validaCPF(cpf)) {
-        alert("CPF válido!");
-        document.getElementById("enviar").disabled = false;
-    } else {
-        alert("CPF inválido!");
-        document.getElementById("enviar").disabled = true;
-        document.getElementById("cpfProprietario").value = '';
-    }
+  var strCPF = String(cpfProprietario).replace(/[^\d]/g, '')
+
+  if (strCPF.length !== 11)
+    return false
+
+  if ([
+    '00000000000',
+    '11111111111',
+    '22222222222',
+    '33333333333',
+    '44444444444',
+    '55555555555',
+    '66666666666',
+    '77777777777',
+    '88888888888',
+    '99999999999',
+  ].indexOf(strCPF) !== -1)
+    return false
+
+  for (i = 1; i <= 9; i++)
+    Soma = Soma + parseInt(strCPF.substring(i - 1, i)) * (11 - i);
+
+  Resto = (Soma * 10) % 11
+
+  if ((Resto == 10) || (Resto == 11))
+    Resto = 0
+
+  if (Resto != parseInt(strCPF.substring(9, 10)))
+    return false
+
+  Soma = 0
+
+  for (i = 1; i <= 10; i++)
+    Soma = Soma + parseInt(strCPF.substring(i - 1, i)) * (12 - i)
+
+  Resto = (Soma * 10) % 11
+
+  if ((Resto == 10) || (Resto == 11))
+    Resto = 0
+
+  if (Resto != parseInt(strCPF.substring(10, 11)))
+    return false
+
+  return true
+}
+function verificarCPF() {
+  var cpfInput = document.getElementById("cpfProprietario");
+  var cpf = cpfInput.value;
+
+  if (validaCPF(cpf)) {
+    alert("CPF válido!");
+    document.getElementById("enviar").disabled = false;
+  } else {
+    alert("CPF inválido!");
+    document.getElementById("enviar").disabled = true;
+    document.getElementById("cpfProprietario").value = '';
+  }
 }
 
 
 function formatarCPF() {
-    var cpf = document.getElementById('cpfProprietario');
-    var valor = cpf.value.replace(/\D/g, '');
+  var cpf = document.getElementById('cpfProprietario');
+  var valor = cpf.value.replace(/\D/g, '');
 
-    if (valor.length > 0) {
-        valor = valor.replace(/(\d{3})(\d)/, '$1.$2');
-        valor = valor.replace(/(\d{3})(\d)/, '$1.$2');
-        valor = valor.replace(/(\d{3})(\d{2})$/, '$1-$2');
-    }
+  if (valor.length > 0) {
+    valor = valor.replace(/(\d{3})(\d)/, '$1.$2');
+    valor = valor.replace(/(\d{3})(\d)/, '$1.$2');
+    valor = valor.replace(/(\d{3})(\d{2})$/, '$1-$2');
+  }
 
-    cpf.value = valor;
+  cpf.value = valor;
 
 }
 
 
-function formatarTelefone(){
+function formatarTelefone() {
   var telefone = document.getElementById('telefone');
   var valorTelefone = telefone.value.replace(/\D/g, '');
   if (valorTelefone.length > 0) {
     valorTelefone = '(' + valorTelefone.substring(0, 2) + ') ' +
-    valorTelefone.substring(2, 7) + '-' +
-     valorTelefone.substring(7, 11);
-  
+      valorTelefone.substring(2, 7) + '-' +
+      valorTelefone.substring(7, 11);
+
   }
-  return telefone.value=valorTelefone;
+  return telefone.value = valorTelefone;
 }
 
 
@@ -140,32 +140,33 @@ function formatarMoeda(input) {
 
   // Formata o valor como moeda brasileira
   valor = (parseFloat(valor) / 100).toLocaleString('pt-BR', {
-      style: 'currency',
-      currency: 'BRL'
+    style: 'currency',
+    currency: 'BRL'
   });
 
   input.value = valor;
 }
 
-  async function modalRevisao(){
-    await fetch()
-    
-  
-  }
+async function modalRevisao() {
+  var dataFinal = document.getElementById("dataFinal").value;
+  var dataInicial = document.getElementById("dataInicial").value;
 
-  /**selectProprietario.onchange = function () {
-    // console.log('...', document.getElementById('campo2').value)
-    let selectVeiculo = document.getElementById('proprietarioRevisao');
-    let valor = selectVeiculo.value;
-    //        console.log('valor',valor);
-    fetch("get-veiculos.php?proprietario_cpf=" + valor)
-        .then(response => {
-            return response.text();
-        })
-        .then(texto => {
-            //console.log(texto)
-            document.getElementById('veiculoPropietario').innerHTML = texto;
-        });
+  await fetch('get-revisao.php?dataInicial=' + dataInicial + '&dataFinal=' + dataFinal);
+}
+
+
+/**selectProprietario.onchange = function () {
+  // console.log('...', document.getElementById('campo2').value)
+  let selectVeiculo = document.getElementById('proprietarioRevisao');
+  let valor = selectVeiculo.value;
+  //        console.log('valor',valor);
+  fetch("get-veiculos.php?proprietario_cpf=" + valor)
+      .then(response => {
+          return response.text();
+      })
+      .then(texto => {
+          //console.log(texto)
+          document.getElementById('veiculoPropietario').innerHTML = texto;
+      });
 
 } */
-  
